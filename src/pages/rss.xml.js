ed73284f8df71f145d05f4b00bbb5fcd4ab107f7
Steve Occhipinti <dev@stevenocchipinti.com>
2024-01-16 19:47:49 +1100

    Fix the RSS feed
    This was still based on the original blog collection, which was adjusted in the
    last commit to get rid of unnecessary frontmatter fields

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,15 @@ import { getCollection } from "astro:content"
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts"
 
 export async function GET(context) {
-  const posts = await getCollection("blog")
+  const posts = await getCollection("travel")
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map(post => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      link: `/travel/${post.slug}/`,
     })),
   })
 }
